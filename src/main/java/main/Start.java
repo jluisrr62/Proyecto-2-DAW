1ae70979d9f03873c16fa6ab98b9e30708e5bc88
Jose Luis <jluisrr62@educastur.es>
2022-10-25 14:26:00 +0200

    cambios al persistence y añadidos metodos al dao

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -1,10 +1,12 @@
 package main;
 
+import modelo.Alumno;
+
 public class Start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Alumno a1 = new Alumno("Juan");
 	}
 
 }
--- src/main/java/modeloDAO/AlumnoDAO.java
+++ src/main/java/modeloDAO/AlumnoDAO.java
@@ -1,5 +1,119 @@
 package modeloDAO;
 
-public class AlumnoDAO {
+import java.util.ArrayList;
 
+import javax.persistence.PersistenceException;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import modelo.Alumno;
+import utils.HibernateUtil;
+
+public class AlumnoDAO {
+	public void insertarAlumnoHibernate(Alumno a) {
+		Transaction tr = null;
+		Session sesion = null;
+
+		try {
+
+			sesion = HibernateUtil.getSessionFactory().openSession();
+			tr = sesion.beginTransaction();
+			sesion.persist(a);
+			tr.commit();
+
+		} catch (PersistenceException e) {
+			tr.rollback();
+
+		} finally {
+			sesion.close();
+		}
+	}
+	
+	public void modificarAlumnoHibernate(Alumno a) {
+		Transaction tr = null;
+		Session sesion = null;
+		
+		try {
+
+			sesion = HibernateUtil.getSessionFactory().openSession();
+			tr = sesion.beginTransaction();
+			sesion.merge(a);
+			tr.commit();
+
+		} catch (PersistenceException e) {
+			tr.rollback();
+
+		} finally {
+			sesion.close();
+		}
+	}
+	
+	public void deleteAlumnoHibernate(Alumno a) {
+		Transaction tr = null;
+		Session sesion = null;
+		
+		try {
+
+			sesion = HibernateUtil.getSessionFactory().openSession();
+			tr = sesion.beginTransaction();
+			sesion.remove(a);
+			tr.commit();
+
+		} catch (PersistenceException e) {
+			tr.rollback();
+
+		} finally {
+			sesion.close();
+		}
+	}
+	
+	public ArrayList<Alumno> listarAlumnosHibernate() {
+		ArrayList<Alumno> resultado = null;
+		
+		Transaction tr = null;
+		Session sesion = null;
+		
+		try {
+
+			sesion = HibernateUtil.getSessionFactory().openSession();
+			tr = sesion.beginTransaction();
+			resultado = (ArrayList<Alumno> ) sesion.createQuery("from Alumno").getResultList();
+			tr.commit();
+			
+			return resultado;
+
+		} catch (PersistenceException e) {
+			tr.rollback();
+
+		} finally {
+			sesion.close();
+		}
+		
+		return null;
+	}
+	
+	public Alumno buscarPorIdHibernate(int i) {
+		
+		Transaction tr = null;
+		Session sesion = null;
+		
+		try {
+
+			sesion = HibernateUtil.getSessionFactory().openSession();
+			tr = sesion.beginTransaction();
+			Alumno miAlumno = (Alumno) sesion.find(Alumno.class, i);
+			System.out.println("El alumno buscado se llama " + miAlumno.getNombre());
+			tr.commit();
+			return miAlumno;
+
+		} catch (PersistenceException e) {
+			tr.rollback();
+
+		} finally {
+			sesion.close();
+		}
+		
+		return null;
+	}
 }
