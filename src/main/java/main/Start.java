7e5e919e01992c9b00b55be44eb2bdcb85d1730e
Jose <jluisrr62@educastur.es>
2022-11-25 18:14:38 +0100

    añadida relacion entre admin y libro y añadida clase asignatura

--- src/main/java/main/Start.java
+++ src/main/java/main/Start.java
@@ -2,7 +2,7 @@ package main;
 
 import modelo.Admin;
 import modelo.Alumno;
-
+import modelo.Asignatura;
 import modelo.Libro;
 import modelo.Recogida;
 import modelo.Usuario;
@@ -14,16 +14,27 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Alumno javi = new Alumno("734265B","Javier", "javierito", "123javier");
-		Usuario jorjito = new Admin("717255C","Jorge", "47891494", "jorjito67", "123456");
-		
-		GenericDAO<Usuario> aDAO = new GenericDAO<Usuario>(Usuario.class);
-		GenericDAO<Libro> bDAO = new GenericDAO<Libro>(Libro.class);
-		GenericDAO<Recogida> cDAO = new GenericDAO<Recogida>(Recogida.class);
+		Admin jorjito = new Admin("717255C","Jorge", "47891494", "jorjito67", "123456");
 		
 		Libro mates = new Libro("owghuiorwghb", "matematicas");
 		Libro lengua = new Libro("weibikr","lengua");
 		
 		Recogida recogida1 = new Recogida();
+		
+		Asignatura mates2 = new Asignatura("Mates","2Bachiller");
+		Asignatura lengua1 = new Asignatura("Lengua","2Bachiller");
+		
+		GenericDAO<Usuario> aDAO = new GenericDAO<Usuario>(Usuario.class);
+		GenericDAO<Libro> bDAO = new GenericDAO<Libro>(Libro.class);
+		GenericDAO<Recogida> cDAO = new GenericDAO<Recogida>(Recogida.class);
+		GenericDAO<Asignatura> dDao = new GenericDAO<Asignatura>(Asignatura.class);
+		
+		mates2.getLibros().add(mates);
+		mates.setAsignatura(mates2);
+		
+		lengua1.getLibros().add(lengua);
+		lengua.setAsignatura(lengua1);
+		
 		recogida1.setFecha(recogida1.parseFecha("2022-06-17"));
 		recogida1.setHora(recogida1.parseHora("10:30"));
 		
@@ -33,8 +44,11 @@ public class Start {
 		mates.getRecogidas().add(recogida1);
 		recogida1.setLibro(mates);
 		
-		
+		lengua.getAdmins().add(jorjito);
+		jorjito.getLibros().add(lengua);
 	
+		dDao.insertarRegistroHibernate(mates2);
+		dDao.insertarRegistroHibernate(lengua1);
 		bDAO.insertarRegistroHibernate(mates);
 		bDAO.insertarRegistroHibernate(lengua);
 		aDAO.insertarRegistroHibernate(javi);
--- src/main/java/modelo/Libro.java
+++ src/main/java/modelo/Libro.java
@@ -3,11 +3,17 @@ package modelo;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -24,13 +30,34 @@ public class Libro {
 	@Column(name = "nombre")
 	private String nombre;
 	
+	@ManyToMany(cascade = {CascadeType.MERGE}, fetch = FetchType.EAGER)
+	@JoinTable(
+			name = "admins_libros",
+			joinColumns = {@JoinColumn(name= "id_libro")},
+			inverseJoinColumns = {@JoinColumn(name="id_admin")}
+		)
+	private Set<Admin> admins;
+	
 	@OneToMany(mappedBy="libro")
 	Set<Recogida> recogidas;
+	
+	@ManyToOne
+	@JoinColumn(name="asignatura_id")
+	Asignatura asignatura;
 	
 	public Libro(String ISBN, String nombre){
 		this.ISBN = ISBN;
 		this.nombre = nombre;
 		this.recogidas =  new HashSet<Recogida>();
+		this.admins = new HashSet<Admin>();
+	}
+
+	public Asignatura getAsignatura() {
+		return asignatura;
+	}
+
+	public void setAsignatura(Asignatura asignatura) {
+		this.asignatura = asignatura;
 	}
 
 	public String getISBN() {
@@ -57,7 +84,23 @@ public class Libro {
 		this.recogidas = recogidas;
 	}
 
+	public int getId() {
+		return id;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Set<Admin> getAdmins() {
+		return admins;
+	}
+
+	public void setAdmins(Set<Admin> admins) {
+		this.admins = admins;
+	}
+
+	
 	
 	
 }
